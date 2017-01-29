2433328f67ffabbf85d82213e1ac9630bf0ad4f2
Luke Stoward <luke.stoward@students.plymouth.ac.uk>
2017-01-29 18:04:52 +0000

    Bank Transfer Service project created
    A new service project (Web API 2) has been created using the default set
    up. The same custom filter has been added to validate the model state
    before reaching a controller action.

---
+++ Services/ABC_Banking.Services.BankTransfer/ABC_Banking.Services.BankTransfer/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using ABC_Banking.Services.BankTransfer.Filters;
+
+namespace ABC_Banking.Services.BankTransfer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Filters.Add(new ValidateModelAttribute());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
